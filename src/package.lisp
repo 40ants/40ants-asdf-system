916ff3d406183125e7751035dff4383615ad5f35
Alexander Artemenko <svetlyak.40wt@gmail.com>
2022-11-07 17:09:11 +0300

    Fixed dependency on version package.

--- src/package.lisp
+++ src/package.lisp
@@ -1,6 +1,4 @@
 (uiop:define-package #:40ants-asdf-system
   (:use #:cl)
-  (:import-from #:40ants-asdf-system/version
-                #:extract-version-from-changelog)
   (:nicknames #:40ants-asdf-system/system))
 
