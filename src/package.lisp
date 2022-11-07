0296d35135eab45ed8d60a5923dac802bfd9d0a4
Alexander Artemenko <svetlyak.40wt@gmail.com>
2022-11-07 17:00:23 +0300

    Added missing files.

---
+++ src/package.lisp
@@ -0,0 +1,6 @@
+(uiop:define-package #:40ants-asdf-system
+  (:use #:cl)
+  (:import-from #:40ants-asdf-system/version
+                #:extract-version-from-changelog)
+  (:nicknames #:40ants-asdf-system/system))
+
