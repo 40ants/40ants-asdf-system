5f9a41c9755755f86baa4d36bbeb7837471beef7
Alexander Artemenko <svetlyak.40wt@gmail.com>
2023-07-24 00:59:50 +0300

    Version 0.2.0

--- src/changelog.lisp
+++ src/changelog.lisp
@@ -8,5 +8,7 @@
 (defchangelog (:ignore-words ("SLY"
                               "REPL"
                               "HTTP"))
+  (0.2.0 2023-07-23
+         "* Now system version number is recorded after the load-op operation. This should fix NIL version under ASDF 3.3.1, because seems it does not call define-op somtimes.")
   (0.1.0 2022-08-07
          "* Initial version."))
